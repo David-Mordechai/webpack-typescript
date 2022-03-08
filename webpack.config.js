b07988b72680c308d871daddb3ac727d01248f18
roi.peretz <roi081295@gmail.com>
2022-03-08 11:16:45 +0200

    added cesium package + webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require("path");
 const CopyPlugin = require('copy-webpack-plugin');
+const cesiumSource = 'node_modules/cesium/Source';
+const cesiumWorkers = '../Build/Cesium/Workers';
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.ts",
@@ -16,22 +20,46 @@ module.exports = {
             },
             {
                 test: /\.css$/i,
-                include: path.resolve(__dirname, "src"),
                 use: ["style-loader", "css-loader"],
             },
+            {
+                test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+                use: [ 'url-loader' ]
+            }
         ],
     },
     resolve:{
         extensions: [".tsx", ".ts", ".js"],
+        alias: {
+            cesium: path.resolve(__dirname, cesiumSource)
+        },
+        mainFiles: ['index', 'Cesium']
     },
     plugins:[
-        new CopyPlugin({
-            patterns: [{ from: "src/index.html", to: "index.html"}],
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
         }),
+        new CopyPlugin({
+            patterns: [
+                //{ from: "src/index.html", to: "index.html"},
+                { from: path.join(cesiumSource, cesiumWorkers), to: 'Workers' },
+                { from: path.join(cesiumSource, 'Assets'), to: 'Assets' },
+                { from: path.join(cesiumSource, 'Widgets'), to: 'Widgets' }
+            ],
+        }),
+        new webpack.DefinePlugin({
+            // Define relative base path in cesium for loading assets
+            CESIUM_BASE_URL: JSON.stringify('')
+        })
     ],
     output:{
         filename: 'bundle.js',
         path: path.resolve(__dirname, "dist"),
         clean: true,
+        sourcePrefix: ''
+    },
+    amd: {
+        // Enable webpack-friendly use of require in Cesium
+        toUrlUndefined: true
     },
 };
